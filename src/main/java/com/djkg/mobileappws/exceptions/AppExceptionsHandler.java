0c953d91212b2b8d906d63994ab78aadba187ca1
Ken Gardiner <kenneth.gardiner@libertymutual.com>
2019-07-23 15:07:46 -0400

    Added custom errormessage

--- src/main/java/com/djkg/mobileappws/exceptions/AppExceptionsHandler.java
+++ src/main/java/com/djkg/mobileappws/exceptions/AppExceptionsHandler.java
@@ -8,14 +8,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ui.model.response.ErrorMessage;
 
 import javax.xml.ws.Response;
+import java.util.Date;
 
 @ControllerAdvice
 public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleAnyExceptions(Exception ex, WebRequest request){
+
+        String errorMessageDescription = ex.getLocalizedMessage();
+
+        if(errorMessageDescription == null) errorMessageDescription = ex.toString();
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+
         return new ResponseEntity<>(
-                ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+                errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
